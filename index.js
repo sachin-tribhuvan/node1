711066130ee407fef86e660d7d98ed5eb1037ed5
sachin-tribhuvan <tribhuvansachin80@gmail.com>
2026-01-20 13:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1> Pankaj Tribhuvan</h1>')
+  res.send('<h1> Aarush Kabir </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
