b0074f40399eb681956e20c0b76c023410fc76f0
sachin-tribhuvan <tribhuvansachin80@gmail.com>
2026-01-20 13:56:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1> Hello world</h1>')
+  res.send('<h1> Hello Jenkins </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
